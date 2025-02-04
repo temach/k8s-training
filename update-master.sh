294a5e434e5955e59061d22fa21334a86b66a362
temach <tematibr@gmail.com>
2025-02-04 08:33:13 +0500

    small fixes to upgrade scripts

--- update-master.sh
+++ update-master.sh
@@ -1,20 +1,15 @@
 vim /etc/apt/sources.list.d/kubernetes.list
 # deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /
 
-apt update
-
-apt-mark unhold kubeadm
-apt install -y kubeadm
-apt-mark hold kubeadm
-
+apt-mark unhold kubeadm && apt-get update && apt-get install -y kubeadm && apt-mark hold kubeadm
 kubeadm upgrade plan
 kubeadm upgrade apply v1.32.1
 
-export KUBECONFIG=/etc/kubernetes/admin.conf
-kubectl drain master --ignore-daemonsets
+export KUBECONFIG=/etc/kubernetes/admin.conf  # on master node
+kubectl drain master --ignore-daemonsets  # on master node
 
 apt-mark unhold kubelet kubectl && apt-get update && apt-get install -y kubelet kubectl && apt-mark hold kubelet kubectl
 systemctl daemon-reload
 systemctl restart kubelet
 
-kubectl uncordon master
+kubectl uncordon master  # on master node
--- update-worker.sh
+++ update-worker.sh
@@ -4,11 +4,11 @@ vim /etc/apt/sources.list.d//kubernetes.list
 apt-mark unhold kubeadm && apt-get update && apt-get install -y kubeadm && apt-mark hold kubeadm
 kubeadm upgrade node
 
+export KUBECONFIG=/etc/kubernetes/admin.conf  # on master node
 kubectl drain worker1 --ignore-daemonsets   # on master node
 
 apt-mark unhold kubelet kubectl && apt-get update && apt-get install -y kubelet kubectl && apt-mark hold kubelet kubectl
 systemctl daemon-reload
 systemctl restart kubelet
-
 
 kubectl uncordon worker1   # on master node
